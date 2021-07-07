jasmine/jasmine-browser-runner

commit 79e0c096ca630e48c6c6ac6b51fbf1a4fd58ba74

Support .js config files that are ES modules

lib/loadConfig.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports = function loadConfig(baseDir, cliOptions) {
+module.exports = async function loadConfig(baseDir, cliOptions) {
   const options = { ...cliOptions };
   const specifiedConfigFile = options.config;
   delete options.config;
@@ -33,5 +33,14 @@ module.exports = function loadConfig(baseDir, cliOptions) {
     throw new Error(msg);
   }
 
-  return Object.assign({}, require(fullPath), options);
+  return Object.assign({}, await loadFile(fullPath), options);
 };
+
+async function loadFile(fullPath) {
+  if (fullPath.endsWith('.json')) {
+    return require(fullPath);
+  } else {
+    const module = await import(fullPath);
+    return module.default;
+  }
+}

spec/loadConfigSpec.js
@@ -1,8 +1,8 @@
 const loadConfig = require('../lib/loadConfig');
 
 describe('loadConfig', function() {
-  it('uses a specified config file', function() {
-    const result = loadConfig('.', {
+  it('uses a specified config file', async function() {
+    const result = await loadConfig('.', {
       config: 'spec/fixtures/loadConfig/json/spec/support/jasmine-browser.json',
     });
     expect(result.browser.name).toEqual(
@@ -10,18 +10,25 @@ describe('loadConfig', function() {
     );
   });
 
-  it('throws if the specified config file is missing', function() {
-    expect(function() {
-      loadConfig('.', { config: 'no/such.json' });
-    }).toThrowError(
+  it('can load a config file that is an ES module', async function() {
+    const result = await loadConfig('spec/fixtures/loadConfig/esm', {});
+    expect(result.browser.name).toEqual(
+      'the browser from the es module config'
+    );
+  });
+
+  it('throws if the specified config file is missing', async function() {
+    await expectAsync(
+      loadConfig('.', { config: 'no/such.json' })
+    ).toBeRejectedWithError(
       /^Could not find configuration file.\nTried:\n\* .*no\/such.json/gm
     );
   });
 
   describe('When there is no specified config file', function() {
     describe('and spec/support/jasmine-browser.js is present', function() {
-      it('loads spec/support/jasmine-browser.js', function() {
-        const result = loadConfig('spec/fixtures/loadConfig/both', {});
+      it('loads spec/support/jasmine-browser.js', async function() {
+        const result = await loadConfig('spec/fixtures/loadConfig/both', {});
         expect(result.browser.name).toEqual(
           'the browser from jasmine-browser.js'
         );
@@ -29,18 +36,18 @@ describe('loadConfig', function() {
     });
 
     describe('and spec/support/jasmine-browser.js is absent', function() {
-      it('loads spec/support/jasmine-browser.json', function() {
-        const result = loadConfig('spec/fixtures/loadConfig/json', {});
+      it('loads spec/support/jasmine-browser.json', async function() {
+        const result = await loadConfig('spec/fixtures/loadConfig/json', {});
         expect(result.browser.name).toEqual(
           'the browser from jasmine-browser.json'
         );
       });
 
       describe('and spec/support/jasmine-browser.json is also absent', function() {
-        it('thorws an error', function() {
-          expect(function() {
-            loadConfig('spec/fixtures/loadConfig/none', {});
-          }).toThrowError(
+        it('thorws an error', async function() {
+          await expectAsync(
+            loadConfig('spec/fixtures/loadConfig/none', {})
+          ).toBeRejectedWithError(
             /^Could not find configuration file.\nTried:\n\* .*spec\/fixtures\/loadConfig\/none\/spec\/support\/jasmine-browser.js\n\* .*spec\/fixtures\/loadConfig\/none\/spec\/support\/jasmine-browser.json/
           );
         });

lib/command.js
@@ -156,11 +156,13 @@ class Command {
   }
 
   async serve(options) {
-    await this._config.jasmineBrowser.startServer(this._loadConfig(options));
+    await this._config.jasmineBrowser.startServer(
+      await this._loadConfig(options)
+    );
   }
 
   async runSpecs(options) {
-    await this._config.jasmineBrowser.runSpecs(this._loadConfig(options));
+    await this._config.jasmineBrowser.runSpecs(await this._loadConfig(options));
   }
 
   _loadConfig(options) {

spec/commandSpec.js
@@ -27,7 +27,7 @@ describe('Command', function() {
   });
 
   describe('With no subcommand specified', function() {
-    it('runs the serve subcommand', function() {
+    it('runs the serve subcommand', async function() {
       const fakeJasmineBrowser = jasmine.createSpyObj('jasmineBrowser', [
           'startServer',
         ]),
@@ -37,14 +37,14 @@ describe('Command', function() {
           baseDir: path.resolve(__dirname, 'fixtures/sampleProject'),
         });
 
-      command.run([]);
+      await command.run([]);
 
       expect(fakeJasmineBrowser.startServer).toHaveBeenCalled();
     });
   });
 
   describe('serve', () => {
-    it('starts a server', function() {
+    it('starts a server', async function() {
       const fakeJasmineBrowser = jasmine.createSpyObj('jasmineBrowser', [
           'startServer',
         ]),
@@ -54,7 +54,7 @@ describe('Command', function() {
           baseDir: path.resolve(__dirname, 'fixtures/sampleProject'),
         });
 
-      command.run(['serve', '--config=sampleConfig.json']);
+      await command.run(['serve', '--config=sampleConfig.json']);
 
       const options = require(path.join(
         __dirname,
@@ -64,7 +64,7 @@ describe('Command', function() {
       expect(fakeJasmineBrowser.startServer).toHaveBeenCalledWith(options);
     });
 
-    it('finds a default config when serving', function() {
+    it('finds a default config when serving', async function() {
       const fakeJasmineBrowser = jasmine.createSpyObj('jasmineBrowser', [
           'startServer',
         ]),
@@ -74,7 +74,7 @@ describe('Command', function() {
           baseDir: path.resolve(__dirname, 'fixtures/sampleProject'),
         });
 
-      command.run(['serve']);
+      await command.run(['serve']);
 
       const options = require(path.join(
         __dirname,
@@ -84,7 +84,7 @@ describe('Command', function() {
       expect(fakeJasmineBrowser.startServer).toHaveBeenCalledWith(options);
     });
 
-    it('allows CLI args to override config file when serving', function() {
+    it('allows CLI args to override config file when serving', async function() {
       const fakeJasmineBrowser = jasmine.createSpyObj('jasmineBrowser', [
           'startServer',
         ]),
@@ -94,7 +94,7 @@ describe('Command', function() {
           baseDir: path.resolve(__dirname, 'fixtures/sampleProject'),
         });
 
-      command.run(['serve', '--config=sampleConfig.json', '--port=2345']);
+      await command.run(['serve', '--config=sampleConfig.json', '--port=2345']);
 
       const options = require(path.join(
         __dirname,
@@ -142,7 +142,7 @@ describe('Command', function() {
   });
 
   describe('version', function() {
-    it('reports the version number', function() {
+    it('reports the version number', async function() {
       const jasmineBrowserVersion = require('../package.json').version;
       const command = new Command({
         jasmineBrowser: {},
@@ -150,7 +150,7 @@ describe('Command', function() {
         console: this.console,
       });
 
-      command.run(['version']);
+      await command.run(['version']);
 
       expect(this.writer.output).toContain('jasmine-core v17.42');
       expect(this.writer.output).toContain(
@@ -171,14 +171,14 @@ describe('Command', function() {
     });
 
     describe('When spec/support/jasmine-browser.json does not exist', function() {
-      it('creates the file', function() {
+      it('creates the file', async function() {
         const command = new Command({
           jasmineBrowser: {},
           jasmineCore: {},
           console: this.console,
         });
 
-        command.run(['init']);
+        await command.run(['init']);
 
         const actualContents = fs.readFileSync(
           'spec/support/jasmine-browser.json',
@@ -189,7 +189,7 @@ describe('Command', function() {
     });
 
     describe('When spec/support/jasmine-browser.json already exists', function() {
-      it('does not create the file', function() {
+      it('does not create the file', async function() {
         const command = new Command({
           jasmineBrowser: {},
           jasmineCore: {},
@@ -201,7 +201,7 @@ describe('Command', function() {
           'initial contents'
         );
 
-        command.run(['init']);
+        await command.run(['init']);
 
         const actualContents = fs.readFileSync(
           'spec/support/jasmine-browser.json',
@@ -213,14 +213,14 @@ describe('Command', function() {
   });
 
   describe('help', function() {
-    it('wraps the help text to 80 columns', function() {
+    it('wraps the help text to 80 columns', async function() {
       const command = new Command({
         jasmineBrowser: {},
         jasmineCore: {},
         console: this.console,
       });
 
-      command.run(['help']);
+      await command.run(['help']);
 
       const lines = this.writer.output.split('\n');
       expect(lines.length).toBeGreaterThan(0);

spec/fixtures/loadConfig/esm/spec/support/jasmine-browser.js
@@ -1,0 +1,7 @@
+const config = {
+  "browser": {
+    "name": "the browser from the es module config"
+  }
+};
+
+export default config;

commit 03b43b3c261c9ffac29024bb11f33190e8fa51d1

Fixed config loading on Windows
This was broken in 79e0c09 which added support for loading .js files via
import(). Absolute paths to ES modules are supposed to be URLs rather than
bare file paths, but Node currently only enforces this on Windows so it's
easy to miss.

lib/loadConfig.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const ModuleLoader = require('./moduleLoader');
 
 module.exports = async function loadConfig(baseDir, cliOptions) {
   const options = { ...cliOptions };
@@ -33,14 +34,6 @@ module.exports = async function loadConfig(baseDir, cliOptions) {
     throw new Error(msg);
   }
 
-  return Object.assign({}, await loadFile(fullPath), options);
+  const moduleLoader = new ModuleLoader();
+  return Object.assign({}, await moduleLoader.load(fullPath), options);
 };
-
-async function loadFile(fullPath) {
-  if (fullPath.endsWith('.json')) {
-    return require(fullPath);
-  } else {
-    const module = await import(fullPath);
-    return module.default;
-  }
-}

lib/moduleLoader.js
@@ -1,0 +1,33 @@
+const path = require('path');
+
+class ModuleLoader {
+  constructor(deps) {
+    deps = deps || {};
+    this.require_ = deps.requireShim || requireShim;
+    this.import_ = deps.importShim || importShim;
+  }
+
+  async load(filePath) {
+    if (filePath.endsWith('.json')) {
+      return this.require_(filePath);
+    } else {
+      // The ES module spec requires absolute import paths to be valid URLs. As
+      // of v16, Node enforces this on Windows but not on other OSes.
+      if (path.isAbsolute(filePath)) {
+        filePath = `file://${filePath}`;
+      }
+      const module = await this.import_(filePath);
+      return module.default;
+    }
+  }
+}
+
+function requireShim(path) {
+  return require(path);
+}
+
+function importShim(path) {
+  return import(path);
+}
+
+module.exports = ModuleLoader;

spec/loadConfigSpec.js
@@ -21,7 +21,7 @@ describe('loadConfig', function() {
     await expectAsync(
       loadConfig('.', { config: 'no/such.json' })
     ).toBeRejectedWithError(
-      /^Could not find configuration file.\nTried:\n\* .*no\/such.json/gm
+      /^Could not find configuration file.\r?\nTried:\r?\n\* .*no[/\\]such.json/gm
     );
   });
 
@@ -44,11 +44,11 @@ describe('loadConfig', function() {
       });
 
       describe('and spec/support/jasmine-browser.json is also absent', function() {
-        it('thorws an error', async function() {
+        it('throws an error', async function() {
           await expectAsync(
             loadConfig('spec/fixtures/loadConfig/none', {})
           ).toBeRejectedWithError(
-            /^Could not find configuration file.\nTried:\n\* .*spec\/fixtures\/loadConfig\/none\/spec\/support\/jasmine-browser.js\n\* .*spec\/fixtures\/loadConfig\/none\/spec\/support\/jasmine-browser.json/
+            /^Could not find configuration file.\r?\nTried:\r?\n\* .*spec[\\/]fixtures[\\/]loadConfig[\\/]none[\\/]spec[\\/]support[\\/]jasmine-browser.js\n\* .*spec[\\/]fixtures[\\/]loadConfig[\\/]none[\\/]spec[\\/]support[\\/]jasmine-browser.json/
           );
         });
       });

spec/moduleLoaderSpec.js
@@ -1,0 +1,40 @@
+const ModuleLoader = require('../lib/moduleLoader');
+
+describe('ModuleLoader', function() {
+  it('loads json files using require', async function() {
+    const requireShim = jasmine.createSpy('requireShim');
+    const importShim = jasmine.createSpy('importShim');
+    const subject = new ModuleLoader({ requireShim, importShim });
+    const result = { the: 'payload' };
+    requireShim.and.returnValue(result);
+
+    await expectAsync(subject.load('foo.json')).toBeResolvedTo(result);
+    expect(requireShim).toHaveBeenCalledWith('foo.json');
+    expect(importShim).not.toHaveBeenCalled();
+  });
+
+  it('loads non-json files using import', async function() {
+    const requireShim = jasmine.createSpy('requireShim');
+    const importShim = jasmine.createSpy('importShim');
+    const subject = new ModuleLoader({ requireShim, importShim });
+    const result = { the: 'payload' };
+    importShim.and.returnValue(Promise.resolve({ default: result }));
+
+    await expectAsync(subject.load('foo.js')).toBeResolvedTo(result);
+    expect(importShim).toHaveBeenCalledWith('foo.js');
+    expect(requireShim).not.toHaveBeenCalled();
+  });
+
+  describe('When a non-json file has an absolute path', function() {
+    it('converts the path to a URL', async function() {
+      const requireShim = jasmine.createSpy('requireShim');
+      const importShim = jasmine.createSpy('importShim');
+      const subject = new ModuleLoader({ requireShim, importShim });
+      importShim.and.returnValue(new Promise(function() {}));
+
+      subject.load('/path/to/foo.js');
+
+      expect(importShim).toHaveBeenCalledWith('file:///path/to/foo.js');
+    });
+  });
+});