a3342e128d19c40409194286e55923b2049b4108
Steve Gravrock <sdg@panix.com>
2024-05-18 09:34:00 -0700

    Prettier

--- spec/importMapSpec.js
+++ spec/importMapSpec.js
@@ -3,26 +3,30 @@
 const { runJasmine } = require('./integrationSupport');
 
 describe('Import Map Sample Project', function() {
-  it('executes example specs', async function() {
-    const { exitCode, stdout, stderr } = await runJasmine(
-      'spec/fixtures/importMap'
-    );
-
-    if (exitCode === 0) {
-      expect(stdout).toContain('3 specs, 0 failures');
-    } else if (exitCode === 3) {
-      // A normal suite failure. Just include the output.
-      fail(`Child suite failed with output:\n${stdout}`);
-    } else {
-      // Some kind of unexpected failure happened. Include all the info
-      // that we have.
-      fail(
-        `Child suite failed with exit code:\n${exitCode}\n\n` +
-          `stdout:\n${stdout}\n\n` +
-          `stderr:\n${stderr}`
+  it(
+    'executes example specs',
+    async function() {
+      const { exitCode, stdout, stderr } = await runJasmine(
+        'spec/fixtures/importMap'
       );
-    }
-  }, 30 * 1000);
+
+      if (exitCode === 0) {
+        expect(stdout).toContain('3 specs, 0 failures');
+      } else if (exitCode === 3) {
+        // A normal suite failure. Just include the output.
+        fail(`Child suite failed with output:\n${stdout}`);
+      } else {
+        // Some kind of unexpected failure happened. Include all the info
+        // that we have.
+        fail(
+          `Child suite failed with exit code:\n${exitCode}\n\n` +
+            `stdout:\n${stdout}\n\n` +
+            `stderr:\n${stderr}`
+        );
+      }
+    },
+    30 * 1000
+  );
 
   it(
     'executes example specs with moduleRootDir',
