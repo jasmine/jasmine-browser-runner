b41b2f347713ec18b9843c80636ac6596bc09573
Steve Gravrock <sdg@panix.com>
2025-02-11 21:06:15 -0800

    Code style tweaks

--- spec/fixtures/modulesWithSideEffectsInSrcFiles/spec/aSpec.js
+++ spec/fixtures/modulesWithSideEffectsInSrcFiles/spec/aSpec.js
@@ -1,7 +1,10 @@
 it('verifies that ES modules in sources are automatically loaded in the correct order', function() {
-  expect(window._moduleWithSyncSideEffectLoaded).withContext('window._moduleWithSyncSideEffectLoaded').toBe(true);
-  expect(window._scriptSyncSuccess).withContext('window._scriptSyncSuccess').toBe(true);
+  expect(window._moduleWithSyncSideEffectLoaded)
+    .withContext('window._moduleWithSyncSideEffectLoaded').toBe(true);
+  expect(window._scriptSyncSuccess)
+    .withContext('window._scriptSyncSuccess').toBe(true);
 });
+
 it('verifies that ES modules in sources are loaded only once', function() {
   return window.secondaryModuleLoading.then(() => {
     expect(window._moduleRunCount).withContext('window._moduleRunCount').toBe(1);
--- spec/fixtures/modulesWithSideEffectsInSrcFiles/src/scriptNeedsSideEffect.js
+++ spec/fixtures/modulesWithSideEffectsInSrcFiles/src/scriptNeedsSideEffect.js
@@ -1,15 +1,12 @@
-if(window._moduleWithSyncSideEffectLoaded) {
-    window._scriptSyncSuccess = true;
-}else{
-    window._scriptSyncSuccess = false;
-}
+window._scriptSyncSuccess = window._moduleWithSyncSideEffectLoaded;
 
 let resolve;
 window.secondaryModuleLoading = new Promise((intresolve) => {
-    resolve = intresolve;
+  resolve = intresolve;
 });
+
 // Load again to check if module is executed again or not (when loaded from the same path)
 import('myPackage/moduleWithSideEffects.mjs').then((_module) => {
-    // We are done
-    resolve();
+  // We are done
+  resolve();
 });
